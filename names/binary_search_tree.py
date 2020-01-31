9b6bbc269f9d75b7a3307a4f8c0fd9d8c6d786b9
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-01-31 11:53:29 -0600

    adds binary_search_tree

---
+++ names/binary_search_tree.py
@@ -0,0 +1,58 @@
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        # compare root nodes
+        # if lesser, go to left child
+        # if greater or = go to right child
+        # if no child, on that side, insert
+        # else try again starting from the child on the appropriate side
+        if value < self.value:
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        # Look at root, if root is it, return
+        # if value is less than node, go lefft and repeat
+        # If no left child, return none
+        # if value is >= node go right and repeat
+        # if no right child return none
+        if self.value == target:
+            return True
+        elif target < self.value:
+            if self.left is None:
+                return False
+            return self.left.contains(target)
+        elif target >= self.value:
+            if self.right is None:
+                return False
+            return self.right.contains(target)
+
+    # Return the maximum value found in the tree
+    def get_max(self):
+        # go right until no more right
+        if self.right is None:
+            return self.value
+        return self.right.get_max()
+
+    # Call the function `cb` on the value of each node
+    # You may use a recursive or iterative approach
+    def for_each(self, cb):
+        cb(self.value)
+        if self.right:
+            self.right.for_each(cb)
+        if self.left:
+            self.left.for_each(cb)
